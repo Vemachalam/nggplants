1bf030aef37712577b0ab6865ce494502eb82e1b
koteswararao-chandanam <ch.koteswararao431@gmail.com>
2025-12-15 22:59:17 +0530

    NGG008: Latest Announcement and Updates page added.

--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -6,18 +6,19 @@ import { AboutComponent } from './landing/about.component/about.component';
 import { TermsConditionsComponent } from './landing/terms-conditions.component/terms-conditions.component';
 import { SupportnggComponent } from './landing/supportngg.component/supportngg.component';
 import { FranchiseComponent } from './landing/franchise.component/franchise.component';
+import { LatestannouncementUpdates } from './landing/latestannouncement-updates/latestannouncement-updates';
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     children: [
-     
       { path: 'about', component: AboutComponent },
       { path: 'overview', component: OverviewComponent },
       { path: 'Terms-Conditions', component: TermsConditionsComponent},
       { path: 'nggsupport', component: SupportnggComponent },
       { path: 'Franchise', component: FranchiseComponent },
-  ],
+      { path: 'latestannouncement-updates', component: LatestannouncementUpdates}
+    ],
   },
   { path: '**', redirectTo: '' },
 ];
--- frontend/src/app/landing/home.component/home.component.ts
+++ frontend/src/app/landing/home.component/home.component.ts
@@ -5,9 +5,10 @@ import {
   Inject,
   PLATFORM_ID,
   Renderer2,
-  ViewChild
+  ViewChild,
 } from '@angular/core';
-
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-home.component',
   standalone: false,
@@ -15,41 +16,45 @@ import {
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
   @ViewChild('preloader') preloader!: ElementRef;
   @ViewChild('sidebarBtn') sidebarBtn!: ElementRef;
-
   sMenuVisible = false;
   ShowTendersPopup = false;
   isSidebarButtonActive = false;
   isMenuVisible = false;
   isSidebarActive = false;
   isBrowser = false;
-
+  isHomePage = false;
+  
   constructor(
     private renderer: Renderer2,
-    @Inject(PLATFORM_ID) private platformId: Object
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private router: Router
   ) {
     this.isBrowser = isPlatformBrowser(this.platformId);
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.isHomePage = event.urlAfterRedirects === '/';
+      });
   }
-
   ngOnInit() {
     this.setFontSize('medium');
- if (this.isBrowser) {
+    if (this.isBrowser) {
       const dot = document.getElementById('cursor-dot') as HTMLElement | null;
-      const plant = document.getElementById('cursor-plant') as HTMLElement | null;
+      const plant = document.getElementById(
+        'cursor-plant'
+      ) as HTMLElement | null;
 
-     if (!dot || !plant) return;
+      if (!dot || !plant) return;
       let plantX = 0;
       let plantY = 0;
-document.addEventListener('mousemove', (e: MouseEvent) => {
+      document.addEventListener('mousemove', (e: MouseEvent) => {
         const x = e.clientX;
         const y = e.clientY;
         dot.style.transform = `translate(${x}px, ${y}px)`;
-
         plantX += (x - plantX) * 0.15;
         plantY += (y - plantY) * 0.15;
-
         plant.style.transform = `translate(${plantX}px, ${plantY}px)`;
       });
     }
@@ -70,47 +75,41 @@ document.addEventListener('mousemove', (e: MouseEvent) => {
 
   setFontSize(size: 'small' | 'medium' | 'large') {
     if (!this.isBrowser) return;
-
     const body = document.body;
-
     this.renderer.removeClass(body, 'font-small');
     this.renderer.removeClass(body, 'font-medium');
     this.renderer.removeClass(body, 'font-large');
-
     this.renderer.addClass(body, `font-${size}`);
   }
   toggleDropdown(event: MouseEvent, type: 'first' | 'second'): void {
-  const clicked = event.currentTarget as HTMLElement;
+    const clicked = event.currentTarget as HTMLElement;
+    const activeClass = 'active';
+    const targetList = type === 'first' ? 'ul' : '.submenu-list';
+    const siblings = clicked.parentElement?.parentElement?.children;
+    clicked.classList.toggle(activeClass);
+    const next = clicked.nextElementSibling as HTMLElement;
+    if (next && next.matches(targetList)) {
+      next.style.display = next.style.display === 'block' ? 'none' : 'block';
+    }
 
-  const activeClass = 'active';
-  const targetList = type === 'first' ? 'ul' : '.submenu-list';
-  const siblings = clicked.parentElement?.parentElement?.children;
-  clicked.classList.toggle(activeClass);
- const next = clicked.nextElementSibling as HTMLElement;
-  if (next && next.matches(targetList)) {
-    next.style.display = next.style.display === 'block' ? 'none' : 'block';
+    if (siblings) {
+      Array.from(siblings).forEach((sibling) => {
+        if (sibling !== clicked.parentElement) {
+          const sibDropdown = sibling.querySelector<HTMLElement>(targetList);
+          const sibIcon = sibling.querySelector<HTMLElement>('.' + activeClass);
+
+          if (sibDropdown) sibDropdown.style.display = 'none';
+          if (sibIcon) sibIcon.classList.remove(activeClass);
+        }
+      });
+    }
   }
 
-  if (siblings) {
-    Array.from(siblings).forEach((sibling) => {
-      if (sibling !== clicked.parentElement) {
-        const sibDropdown = sibling.querySelector<HTMLElement>(targetList);
-        const sibIcon = sibling.querySelector<HTMLElement>('.' + activeClass);
-
-        if (sibDropdown) sibDropdown.style.display = 'none';
-        if (sibIcon) sibIcon.classList.remove(activeClass);
-     }
-});
-}
-}
- 
   onMouseEnter(event: MouseEvent, spanEl: HTMLElement): void {
     const target = event.currentTarget as HTMLElement;
     const offset = target.getBoundingClientRect();
-
     const relX = event.pageX - offset.left - window.scrollX;
     const relY = event.pageY - offset.top - window.scrollY;
-
     spanEl.style.top = `${relY}px`;
     spanEl.style.left = `${relX}px`;
   }
@@ -118,10 +117,8 @@ document.addEventListener('mousemove', (e: MouseEvent) => {
   onMouseLeave(event: MouseEvent, spanEl: HTMLElement): void {
     const target = event.currentTarget as HTMLElement;
     const offset = target.getBoundingClientRect();
-
     const relX = event.pageX - offset.left - window.scrollX;
     const relY = event.pageY - offset.top - window.scrollY;
-
     spanEl.style.top = `${relY}px`;
     spanEl.style.left = `${relX}px`;
   }
@@ -130,7 +127,7 @@ document.addEventListener('mousemove', (e: MouseEvent) => {
     const main = document.getElementById('main-content');
     if (main) {
       main.focus();
-      main.scrollIntoView({ behavior: 'smooth' });
-}
-}
+      main.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
